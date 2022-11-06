7545bfb8eba73de1cd20290b8d7d7935b74addbc
Mohammed Alobaidi <37510951+moAlobaidi@users.noreply.github.com>
2022-11-06 09:34:43 -0800

    ughghghghghg spaces in class names wtf bro

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -28,52 +28,54 @@ const Home = () => {
       }}
     >
       <div className="sr-main">
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            width: "93%",
-            marginTop: "80px",
-          }}
-        >
-          <ConnectButton />
-        </div>
-        <section
-          className="container"
-          style={{
-            backgroundColor: "white",
-            marginTop: 30,
-            boxShadow:
-              "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
-          }}
-        >
-          <div style={{ backgroundColor: "white" }}>
-            <h1 style={{ color: "midnightblue", textAlign: "center" }}>
-              Input your NFT
-            </h1>
-          </div>
-          <input
-            type="text"
-            placeholder={"token address"}
-            value={tokenAddress}
-            onChange={(e) => setTokenAddress(e.target.value)}
-            style={{ marginTop: 7 }}
-          />
-          <input
-            type="text"
-            placeholder={"token id"}
-            value={tokenID}
-            onChange={(e) => setTokenID(e.target.value)}
-            style={{ marginTop: 10 }}
-          />
-          <button
-            role="link"
-            onClick={handleSubmit}
-            style={{ backgroundColor: "midnightblue", color: "white" }}
+        <div className={"button thingy"}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              width: "93%",
+              marginTop: "80px",
+            }}
           >
-            Submit
-          </button>
-        </section>
+            <ConnectButton />
+          </div>
+          <section
+            className="container"
+            style={{
+              backgroundColor: "white",
+              marginTop: 30,
+              boxShadow:
+                "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
+            }}
+          >
+            <div style={{ backgroundColor: "white" }}>
+              <h1 style={{ color: "midnightblue", textAlign: "center" }}>
+                Input your NFT
+              </h1>
+            </div>
+            <input
+              type="text"
+              placeholder={"token address"}
+              value={tokenAddress}
+              onChange={(e) => setTokenAddress(e.target.value)}
+              style={{ marginTop: 7 }}
+            />
+            <input
+              type="text"
+              placeholder={"token id"}
+              value={tokenID}
+              onChange={(e) => setTokenID(e.target.value)}
+              style={{ marginTop: 10 }}
+            />
+            <button
+              role="link"
+              onClick={handleSubmit}
+              style={{ backgroundColor: "midnightblue", color: "white" }}
+            >
+              Submit
+            </button>
+          </section>
+        </div>
       </div>
     </div>
   );
