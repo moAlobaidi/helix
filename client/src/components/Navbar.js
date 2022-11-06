f31076c4fa12cea87211fbf7b74b71ac9466b29a
Richa Ray <richaray@Richas-MacBook-Pro.local>
2022-11-06 01:50:46 -0800

    added logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import {useHistory} from "react-router-dom";
 import {ConnectButton} from "@rainbow-me/rainbowkit";
+import Logo from './../../src/Helix.jpeg';
 
 const Navbar = () => {
     return (
-        <div>
+        <div style={{display: "flex", flexDirection: 'row', width: '100%', display: 'flex', justifyContent: 'space-between'}}>
+            <div>
+                    <img src={Logo} alt="React Logo" style={{width: '15%'}}/>
+                </div>
             <a href="https://rigorous-parsnip-bbe.notion.site/Learn-a5f878b12c624bfa95933fe4546b6635">
+    
                 <button style={{backgroundColor: 'white', color: 'white'}}>
                     <h2 style={{color: 'midnightblue'}}>Learn</h2>
-                </button>
+                </button> 
             </a>
-
         </div>
     )
     }
